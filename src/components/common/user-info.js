640272253ddd3ea076f89c2c2661e7e04d60d32c
Fayne <FaynePerera@users.noreply.github.com>
2023-10-09 21:44:40 -0600

    Add files via upload

--- src/components/common/user-info.js
+++ src/components/common/user-info.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import { ReactSession } from 'react-client-session';
+import React, { useState, useEffect } from 'react';
+
+ 
 
 function UserInfo() {
-  const username = ReactSession.get("username");
-  const userInfoForm = ReactSession.get("userInfoForm");
-  console.log(userInfoForm);
+
+ 
+
+  const [user, setUser] = useState({});
+
+  const [username, setUsername] = useState("mviveros");
+
+ 
+
+  const getUserData = async (url) => {
+
+    const response = await fetch(url);
+
+    const jsonData = await response.json();
+
+    setUser(jsonData);
+
+    console.log(jsonData)
+
+  };
+
+ 
+
+ 
+
+ 
+
+  React.useEffect(() => {
+
+    const url = 'https://v70epn01t.tmm.na.corp.toyota.com/EPNS-Service/api/user/'+username
+
+    getUserData(url);
+
+  }, []);
+
+ 
+
+ 
+
+ 
 
   return (
+
     <>
-      <span>User Name is: {userInfoForm.name}</span>
-      <span>User Last Name is: {userInfoForm.lastname}</span>
+
+      <span>User Name is: {username}</span>
+
+      <div>
+
+      <input value={username}   name="username" onChange={e => setUsername(e.target.value)} />
+
+      </div>
+
+      <div>
+
+        <span>User First Name is: {user.firstName}</span>
+
+      </div>
+
     </>
+
   )
+
 }
+
+ 
 
 export default UserInfo;
