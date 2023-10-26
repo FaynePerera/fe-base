3bbf416f339f4a86b25394215d87b0cc62213998
Fayne <FaynePerera@users.noreply.github.com>
2023-10-26 02:14:36 -0500

    Fix datepicker input

--- src/components/common/shipment.js
+++ src/components/common/shipment.js
@@ -3,10 +3,15 @@ import {
     Button,
     Row,
     Col,
+    Ratio,
   } from "react-bootstrap";
 
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import UndoSharpIcon from '@mui/icons-material/UndoSharp';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const Shipment = ({shipment}, key) => {
     const [ promiseDate, setPromiseDate ] = useState()
@@ -29,7 +34,19 @@ const Shipment = ({shipment}, key) => {
                 <td>{shipment.shipqty}</td>
                 <td>{shipment.requiredship}</td>
                 <td>{new Date(shipment.shipduedate).toLocaleDateString()}</td>
-                <td>{promiseDate? new Date(promiseDate).toLocaleDateString() : ""}</td>
+                <td style={{"fontSize" : "2pts"}}>
+                    {promiseDate? 
+                        new Date(promiseDate).toLocaleDateString() 
+                        :  
+                        <LocalizationProvider  dateAdapter={AdapterDayjs}>
+                            <DemoContainer components={['DatePicker']}>
+                                <Ratio aspectRatio={'3x2'}>
+                                    <DatePicker  size="sm"/>
+                                </Ratio>  
+                            </DemoContainer>
+                        </LocalizationProvider>
+                    }
+                </td>
                 <td>{shipment.altpickupcountry || "Same"}</td>
                 <td>{shipment.altpickupzip || "Same"}</td>
                 <td>
